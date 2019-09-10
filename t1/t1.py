11f95ab3a7ca26efd6d11c2ce3b508fc027e89cc
LeonardoRez <leonardorec1@gmail.com>
2019-09-10 06:49:11 -0300

    changes the floyd_steinberg to a more generic approach

--- t1/t1.py
+++ t1/t1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-def floyd_steinberg(img):    
+def floyd_steinberg(img, kernel):    
     result = np.zeros(img.shape)
     h,w = img.shape
     for y in range(h):
@@ -9,25 +9,46 @@ def floyd_steinberg(img):
             if img[y,x] > 128:
                 result[y,x] = 1
             error = img[y,x] - result[y,x]*255
-            if y < h-1:
-                img[y+1,x] += (7/16)*error
-            if y > 0 and x < w-1:
-                img[y-1,x+1] += (3/16)*error
-            if x < w-1:
-                img[y,x+1] += (5/16)*error
-            if y < h-1 and x < w-1:
-                img[y+1,x+1] += (1/16)*error
+
+            kh = kernel.shape[0]
+            kw = kernel.shape[1]
+            x_step = int(kw//2)
+
+            for ky in range(kh):
+                for kx in range((-x_step),(x_step),1):
+                    if ((y+ky) < h) and ((x+kx) >= 0) and ((x+kx) < w):
+                        img[y+ky, x+kx] += kernel[ky, kx+x_step]*error
+
     return result.astype(np.uint8)*255
 
 ####### floyd and steinberg tests #######
-
+kernel = np.array([ [0,0,7],
+                    [3,5,1]])
+kernel = kernel/kernel.sum()
 #monochrome test
 img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
-cv2.imwrite('fs-1.png', floyd_steinberg(img.copy()))
+cv2.imwrite('fs-1-1.png', floyd_steinberg(img.copy(), kernel))
 
 #rgb test
 img = cv2.imread('baboon_colored.png')
-img[:,:,0] = floyd_steinberg(img[:,:,0])
-img[:,:,1] = floyd_steinberg(img[:,:,1])
-img[:,:,2] = floyd_steinberg(img[:,:,2])
-cv2.imwrite('fs-2.png', img)
+img[:,:,0] = floyd_steinberg(img[:,:,0], kernel)
+img[:,:,1] = floyd_steinberg(img[:,:,1], kernel)
+img[:,:,2] = floyd_steinberg(img[:,:,2], kernel)
+cv2.imwrite('fs-1-2.png', img)
+
+####### stevenson and arce tests #######
+kernel = np.array([ [0,0,0,0,0,32,0],
+                    [12,0,26,0,30,0,16],
+                    [0,12,0,26,0,12,0],
+                    [5,0,12,0,12,0,5] ])
+kernel = kernel/kernel.sum()
+#monochrome test
+img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
+cv2.imwrite('fs-2-1.png', floyd_steinberg(img.copy(), kernel))
+
+#rgb test
+img = cv2.imread('baboon_colored.png')
+img[:,:,0] = floyd_steinberg(img[:,:,0], kernel)
+img[:,:,1] = floyd_steinberg(img[:,:,1], kernel)
+img[:,:,2] = floyd_steinberg(img[:,:,2], kernel)
+cv2.imwrite('fs-2-2.png', img)
