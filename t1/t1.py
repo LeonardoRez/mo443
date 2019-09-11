fca6d0f250810d0bd6c88a43d4cd4e2e7fb102b8
LeonardoRez <leonardorec1@gmail.com>
2019-09-11 11:15:42 -0300

    ads a test function to call with more than one image

--- t1/t1.py
+++ t1/t1.py
@@ -1,6 +1,14 @@
 import numpy as np
 import cv2
 
+def cut_limits(value):
+    if value > 255:
+        return 255
+    if value < 0:
+        return 0
+    return value
+
+
 def error_diffusion(img, kernel, alternate=False):    
     result = np.zeros(img.shape)
     k = kernel.copy()
@@ -27,152 +35,95 @@ def error_diffusion(img, kernel, alternate=False):
             for ky in range(kh):
                 for kx in range((-x_step),(x_step),1):
                     if ((y+ky) < h) and ((x+kx) >= 0) and ((x+kx) < w):
-                        img[y+ky, x+kx] += kernel[ky, kx+x_step]*error
+                        img[y+ky, x+kx] = cut_limits( img[y+ky, x+kx] + kernel[ky, kx+x_step]*error )
 
     return result.astype(np.uint8)*255
 
+
+def test(img_path, kernel, label):
+    img = cv2.imread(img_path)
+    img[:,:,0] = error_diffusion(img[:,:,0], kernel)
+    img[:,:,1] = error_diffusion(img[:,:,1], kernel)
+    img[:,:,2] = error_diffusion(img[:,:,2], kernel)
+    cv2.imwrite('o-'+label+'-1.png', img)
+    
+    img = cv2.imread(img_path)
+    img[:,:,0] = error_diffusion(img[:,:,0], kernel, alternate=True)
+    img[:,:,1] = error_diffusion(img[:,:,1], kernel, alternate=True)
+    img[:,:,2] = error_diffusion(img[:,:,2], kernel, alternate=True)
+    cv2.imwrite('o-'+label+'-2.png', img)
+
+
 ########################## floyd and steinberg tests ##########################
+# preparing kernel
 kernel = np.array([ [0,0,7],
                     [3,5,1]])
 kernel = kernel/kernel.sum()
-#monochrome test
-img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
-cv2.imwrite('fs-1-1-1.png', error_diffusion(img.copy(), kernel))
-cv2.imwrite('fs-1-1-2.png', error_diffusion(img.copy(), kernel, alternate=True))
 
-#rgb test
-img = cv2.imread('baboon_colored.png')
-img[:,:,0] = error_diffusion(img[:,:,0], kernel)
-img[:,:,1] = error_diffusion(img[:,:,1], kernel)
-img[:,:,2] = error_diffusion(img[:,:,2], kernel)
-cv2.imwrite('fs-1-2-1.png', img)
-
-img = cv2.imread('baboon_colored.png')
-img[:,:,0] = error_diffusion(img[:,:,0], kernel, alternate=True)
-img[:,:,1] = error_diffusion(img[:,:,1], kernel, alternate=True)
-img[:,:,2] = error_diffusion(img[:,:,2], kernel, alternate=True)
-cv2.imwrite('fs-1-2-2.png', img)
+test('a-baboon_colored.png', kernel, 'fs')
+test('a-monalisa_colored.png', kernel, 'fs')
+test('a-peppers_colored.png', kernel, 'fs')
+test('a-watch_colored.png', kernel, 'fs')
 
 
-# ########################## stevenson and arce tests ##########################
-# kernel = np.array([ [0,0,0,0,0,32,0],
-#                     [12,0,26,0,30,0,16],
-#                     [0,12,0,26,0,12,0],
-#                     [5,0,12,0,12,0,5] ])
-# kernel = kernel/kernel.sum()
-# #monochrome test
-# img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
-# cv2.imwrite('sa-1-1-1.png', error_diffusion(img.copy(), kernel))
-# cv2.imwrite('sa-1-1-2.png', error_diffusion(img.copy(), kernel, alternate=True))
-# 
-# #rgb test
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel)
-# cv2.imwrite('sa-1-2-1.png', img)
-# 
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel, alternate=True)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel, alternate=True)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel, alternate=True)
-# cv2.imwrite('sa-1-2-2.png', img)
-#
-# 
-# ########################## burkes tests  ##########################
-# kernel = np.array([ [0,0,0,8,4],
-#                     [2,4,8,4,2] ])
-# kernel = kernel/kernel.sum()
-# #monochrome test
-# img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
-# cv2.imwrite('b-1-1-1.png', error_diffusion(img.copy(), kernel))
-# cv2.imwrite('b-1-1-2.png', error_diffusion(img.copy(), kernel, alternate=True))
-# 
-# #rgb test
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel)
-# cv2.imwrite('b-1-2-1.png', img)
-# 
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel, alternate=True)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel, alternate=True)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel, alternate=True)
-# cv2.imwrite('b-1-2-2.png', img)
-#
-# 
-# ########################## Sierra tests  ##########################
-# kernel = np.array([ [0,0,0,5,3],
-#                     [2,4,5,4,2],
-#                     [0,2,3,2,0] ])
-# kernel = kernel/kernel.sum()
-# #monochrome test
-# img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
-# cv2.imwrite('si-1-1-1.png', error_diffusion(img.copy(), kernel))
-# cv2.imwrite('si-1-1-2.png', error_diffusion(img.copy(), kernel, alternate=True))
-# 
-# #rgb test
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel)
-# cv2.imwrite('si-1-2-1.png', img)
-# 
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel, alternate=True)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel, alternate=True)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel, alternate=True)
-# cv2.imwrite('si-1-2-2.png', img)
-# 
-# 
-# ########################## Stucki tests  ##########################
-# kernel = np.array([ [0,0,0,8,4],
-#                     [2,4,8,4,2],
-#                     [1,2,4,2,1] ])
-# kernel = kernel/kernel.sum()
-# #monochrome test
-# img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
-# cv2.imwrite('st-1-1-1.png', error_diffusion(img.copy(), kernel))
-# cv2.imwrite('st-1-1-2.png', error_diffusion(img.copy(), kernel, alternate=True))
-# 
-# #rgb test
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel)
-# cv2.imwrite('st-1-2-1.png', img)
-# 
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel, alternate=True)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel, alternate=True)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel, alternate=True)
-# cv2.imwrite('st-1-2-2.png', img)
-# 
-#  
-# ########################## Sierra tests  ##########################
-# kernel = np.array([ [0,0,0,7,5],
-#                     [3,5,7,5,3],
-#                     [1,3,5,3,1] ])
-# kernel = kernel/kernel.sum()
-# #monochrome test
-# img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
-# cv2.imwrite('jjn-1-1-1.png', error_diffusion(img.copy(), kernel))
-# cv2.imwrite('jjn-1-1-2.png', error_diffusion(img.copy(), kernel, alternate=True))
-# 
-# #rgb test
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel)
-# cv2.imwrite('jjn-1-2-1.png', img)
-# 
-# img = cv2.imread('baboon_colored.png')
-# img[:,:,0] = error_diffusion(img[:,:,0], kernel, alternate=True)
-# img[:,:,1] = error_diffusion(img[:,:,1], kernel, alternate=True)
-# img[:,:,2] = error_diffusion(img[:,:,2], kernel, alternate=True)
-# cv2.imwrite('jjn-1-2-2.png', img)
+########################## stevenson and arce tests ##########################
+kernel = np.array([ [0,0,0,0,0,32,0],
+                    [12,0,26,0,30,0,16],
+                    [0,12,0,26,0,12,0],
+                    [5,0,12,0,12,0,5] ])
+kernel = kernel/kernel.sum()
 
 
+test('a-baboon_colored.png', kernel, 'sa')
+test('a-monalisa_colored.png', kernel, 'sa')
+test('a-peppers_colored.png', kernel, 'sa')
+test('a-watch_colored.png', kernel, 'sa')
+
+
+########################## burkes tests  ##########################
+kernel = np.array([ [0,0,0,8,4],
+                    [2,4,8,4,2] ])
+kernel = kernel/kernel.sum()
+
+test('a-baboon_colored.png', kernel, 'b')
+test('a-monalisa_colored.png', kernel, 'b')
+test('a-peppers_colored.png', kernel, 'b')
+test('a-watch_colored.png', kernel, 'b')
+
+
+########################## Sierra tests  ##########################
+kernel = np.array([ [0,0,0,5,3],
+                    [2,4,5,4,2],
+                    [0,2,3,2,0] ])
+kernel = kernel/kernel.sum()
+
+test('a-baboon_colored.png', kernel, 'si')
+test('a-monalisa_colored.png', kernel, 'si')
+test('a-peppers_colored.png', kernel, 'si')
+test('a-watch_colored.png', kernel, 'si')
+
+
+########################## Stucki tests  ##########################
+kernel = np.array([ [0,0,0,8,4],
+                    [2,4,8,4,2],
+                    [1,2,4,2,1] ])
+kernel = kernel/kernel.sum()
+
+test('a-baboon_colored.png', kernel, 'st')
+test('a-monalisa_colored.png', kernel, 'st')
+test('a-peppers_colored.png', kernel, 'st')
+test('a-watch_colored.png', kernel, 'st')
+
+ 
+########################## Jarvis, Judice e Ninke tests  ##########################
+kernel = np.array([ [0,0,0,7,5],
+                    [3,5,7,5,3],
+                    [1,3,5,3,1] ])
+kernel = kernel/kernel.sum()
+
+test('a-baboon_colored.png', kernel, 'jjn')
+test('a-monalisa_colored.png', kernel, 'jjn')
+test('a-peppers_colored.png', kernel, 'jjn')
+test('a-watch_colored.png', kernel, 'jjn')
 
 
